eb20520de200132616c54f29bb73f46f1ba8e766
K M Marziul Karim <marziulrafi@gmail.com>
2025-03-09 12:36:42 +0600

    loop

---
+++ object_loop.js
@@ -0,0 +1,16 @@
+const person = {
+    name : 'Karim',
+    age : 23,
+    profession : 'Developer',
+    salary : 30000
+}
+
+for (let key in person) {
+    console.log(`Key : ${key} --- Value : ${person[key]}`)
+}
+
+console.log("\n")
+
+for (let [key,value] of Object.entries(person)) {
+    console.log(`Key : ${key} --- Value : ${value}`)
+}
